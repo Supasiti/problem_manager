b40a65e82a3ec95bf29ade8194c3cbb92cd2870a
Tharatorn Supasiti <thara@Tharatorns-Mac-mini.local>
2021-02-15 10:39:40 +1100

    refactor toolController

--- controllers/tool_controller.py
+++ controllers/tool_controller.py
@@ -15,32 +15,22 @@ class ToolController():
         self._dependency     = dependency
         self._colour_setting = self._dependency.get_or_register(ColourDict)
         
-        view_data  = self._default_view_data()
-        self.model = ToolStationModel(dynamic_data = view_data) # load model
-        self.view  = ToolStation(self, self.model)              # load view
+        self.model = ToolStationModel(dynamic_data = self._view_data()) # load model
+        self.view  = ToolStation(self, self.model)                      # load view
         self._connect_problem_request()
-
-    def _default_view_data(self):
-        problem   = Problem()
-        grade_str = str(problem.grade)
-        holds     = self._colour_setting.get_hold_colours(grade_str)
-        return ToolDynamicData(holds, problem)
 
     def _connect_problem_request(self):
         problem_request = self._dependency.get(ProblemRequest)
         problem_request.problemToEditChanged.connect(self._on_problem_to_edit_changed)
 
     def _on_problem_to_edit_changed(self, arg:bool):
-        problem_request = self._dependency.get(ProblemRequest)
-        problem_to_edit = problem_request.problem_to_edit
-        if not problem_to_edit is None:
-            self._update_view(problem_to_edit)
-            return True
-        self.model.dynamic_data = self._default_view_data()
+        problem_request         = self._dependency.get(ProblemRequest)
+        problem_to_edit         = problem_request.problem_to_edit
+        self.model.dynamic_data = self._view_data(problem_to_edit)
         return True
 
-    def _update_view(self, problem:Problem):
-        grade_str = str(problem.grade)
+    def _view_data(self, problem:Problem = None):
+        _problem  = Problem() if problem is None else problem
+        grade_str = str(_problem.grade)
         holds     = self._colour_setting.get_hold_colours(grade_str)
-        self.model.dynamic_data = ToolDynamicData(holds, problem)
-        return True
+        return ToolDynamicData(holds, _problem)
